901c0086295a79229763f08348225ae9f0920e92
Oleksii Moiseenko <>
2019-09-18 23:17:23 +0300

    fixes;

--- delivery.go
+++ delivery.go
@@ -7,7 +7,8 @@ type PlainTextDelivery interface {
 }
 
 
-type EmailTemplateDelivery interface {
+type EmailDelivery interface {
+	PlainTextDelivery
 	SendTemplate(ctx context.Context, destination, subject, template string, variables map[string]string)error
 }
 
